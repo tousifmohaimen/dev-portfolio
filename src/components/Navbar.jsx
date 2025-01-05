25a3b43f7ac246aacebb379f6b293f6bf6b16f84
Tousif Mohaimen <mohaimentousif10@gmail.com>
2025-01-05 23:59:19 +0600

    Hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
                     <img src={logo} alt="logo" className="w-9 h-9 object-contain"/>
                     <p className="text-white text-[18px] font-bold cursor-pointer flex">
                         Tousif &nbsp;
-                        <span className="sm:block hidden">| Mohaimen</span>
+                        <span className="sm:block hidden">Mohaimen</span>
                     </p>
                 </Link>
                 <ul className="list-none hidden sm:flex flex-row gap-10">
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, {Suspense} from "react";
+import {Canvas} from "@react-three/fiber";
+import {OrbitControls, Preload, useGLTF} from "@react-three/drei";
+import CanvasLoader from "../Loader";
 
-const Computers = () => {
-  return (
-    <div>Computers</div>
-  )
-}
+const Computers = ({isMobile}) => {
+    const computer = useGLTF("./desktop_pc/scene.gltf");
+    return (
+        <mesh>
+            <hemisphereLight intensity={0.5} groundColor="black"/>
+            <pointLight intensity={2}/>
+            <spotLight
+                position={[-50, 50, 40]}
+                angle={0.2}
+                penumbra={1}
+                intensity={2}
+                castShadow="castShadow"
+                shadow-mapsize={1024}/>
+            <ambientLight intensity={1.3}/>
+            <primitive
+                object={computer.scene}
+                scale={isMobile
+                    ? 0.7
+                    : 0.75}
+                position={isMobile
+                    ? [0, -3.0, -2.2]
+                    : [0, -3.5, -1.5]}
+                rotation={[-0.01, -0.2, -0.1]}/>
+        </mesh>
+    );
+};
 
-export default Computers
+const ComputersCanvas = () => {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+        setIsMobile(mediaQuery.matches);
+
+        const handleMediaQueryChange = (event) => {
+            setIsMobile(event.matches);
+        };
+
+        mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+        return() => {
+            mediaQuery.removeEventListener("change", handleMediaQueryChange);
+        };
+    }, [third]);
+
+    return (
+        <> < Canvas frameloop = "demand" shadows = "shadow" camera = {{
+            position: [20, 3, 5],
+            fov: 25,
+          }}
+        gl = {{
+            preserveDrawingBuffer: true,
+          }} > <Suspense fallback={<CanvasLoader />}>
+            <OrbitControls
+                enableZoom={false}
+                maxPolarAngle={Math.PI / 2}
+                minPolarAngle={Math.PI / 2}/>
+            <Computers isMobile={isMobile}/>
+        </Suspense>
+        <Preload all="all"/>
+    </Canvas>
+</>
+    );
+};
+
+export default ComputersCanvas;
