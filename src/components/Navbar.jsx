b711ff10b49ed243d8be79fd9d0f675b70ae8272
Tousif Mohaimen <mohaimentousif10@gmail.com>
2025-01-12 14:21:33 +0600

    logo fixed in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,8 @@ import React, {useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import {styles} from "../styles";
 import {navLinks} from "../constants";
-import {logo, menu, close} from "../assets";
+import {menu, close} from "../assets";
+import logo from "../assets/logo.png"
 
 const Navbar = () => {
     const [active, setActive] = useState("");
@@ -16,7 +17,7 @@ const Navbar = () => {
                     className="flex items-center gap-2"
                     onClick={() => {
                         setActive("");
-                        window.scrollTo(0, 0);
+                        window.scrollTo(0, 0);  
                     }}>
                     <img src={logo} alt="logo" className="w-9 h-9 object-contain"/>
                     <p className="text-white text-[18px] font-bold cursor-pointer flex">
